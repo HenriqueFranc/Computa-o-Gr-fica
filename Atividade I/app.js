05594d0806612a50279897be34dfd2bd4150275e
HenriqueFranc <henriquealvz@gmail.com>
2021-08-22 10:37:08 -0300

    lendo csv

--- Atividade I/app.js
+++ Atividade I/app.js
@@ -3,28 +3,4 @@
 // posição y do texto: height - 5
 // 40 é o valor de pixels por número de casos
 
-function calcularPosicaoBarra(valor) {
-    return 680 - (valor / 40);
-}
-
-function calcularAlturaBarra(valor) {
-    return (valor / 40);
-}
-
-function calcularPosicaoYTexto(valor) {
-    return 680 - (valor / 40) - 5;
-}
-
-function read () {
-    return fetch('df.csv')
-    .then(response => {
-        return response.text()
-    })
-}
-
-var d = read();
-console.log(d);
- 
-
-
 
